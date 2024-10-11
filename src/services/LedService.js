463ca0355ca3f4f67b2c7e9505504b5c64cba229
Nathan Navarro <nathan.u.navarro@gmail.com>
2024-10-11 02:25:39 +0200

    To clean up, send the proper data to the led. Still no lights up tho

--- src/services/LedService.js
+++ src/services/LedService.js
@@ -2,11 +2,19 @@ import { holdsToLeds, L, C } from '../utils/index.js';
 import { getBoulderById } from './BoulderService.js';
 import ws281x from '@ircam/rpi-ws281x-native';
 
-const channel = ws281x(L * C);
+const channel = ws281x(L * C,{
+  dma: 10,
+  freq: 800000,
+  gpio: 18,
+  invert: false,
+  brightness: 255,
+  stripType: ws281x.stripType.WS2812
+});
 
 export const lightsUpBoulder = async (id) => {
   const boulder = await getBoulderById(id);
-  const leds = holdsToLeds(boulder.holds);
+  console.log('Boulder ?', boulder);
+  const leds = holdsToLeds(boulder);
   channel.array = leds;
   ws281x.render();
   return leds;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
-export const L = 12; // Number of lines
-export const C = 12; // Number of columns
+export const L = 5; // Number of lines
+export const C = 5; // Number of columns
 
 const COLORS = {
   '1': 0x00ff00,
@@ -8,16 +8,17 @@ const COLORS = {
 };
 
 //Careful I think this formula depends on the parity of L. This as been done with a pair L
-export const holdsToLeds = (holds) => {
+export const holdsToLeds = (boulder) => {
   const ledArray = new Uint32Array(L * C);
-
-  Object.keys(holds).forEach((stringHoldIndex) => {
+  console.log('holds in utils', boulder.holds.keys());
+  boulder.holds.keys().forEach((stringHoldIndex) => {
+    console.log('holdindex',stringHoldIndex, boulder.holds.get(stringHoldIndex));
     const holdIndex = parseInt(stringHoldIndex);
     const ledIndex = L * C -
       (Math.ceil(Math.floor(holdIndex / C) / 2)) * 2 * C +
       ((Math.floor(holdIndex / C) + 1) % 2 ? -1 : 1) * (holdIndex % C + (Math.floor(holdIndex / C) + 1) % 2);
 
-    ledArray[ledIndex] = COLORS[holds[stringHoldIndex]];
+    ledArray[ledIndex] = COLORS[boulder.holds.get(stringHoldIndex)];
   });
 
   return ledArray;
