d708cee22d6ebe3f7f2bd24c1703555ca2b4f5a2
Nathan Navarro <nathan.u.navarro@gmail.com>
2024-10-13 16:38:36 +0200

    Use proper leds ref

--- src/services/LedService.js
+++ src/services/LedService.js
@@ -2,13 +2,13 @@ import { holdsToLeds, L, C } from '../utils/index.js';
 import { getBoulderById } from './BoulderService.js';
 import ws281x from '@ircam/rpi-ws281x-native';
 
-const channel = ws281x(L * C,{
+const channel = ws281x(L * C, {
   dma: 10,
   freq: 800000,
   gpio: 18,
   invert: false,
   brightness: 255,
-  stripType: ws281x.stripType.WS2812
+  stripType: ws281x.stripType.WS2811_RGB
 });
 
 export const lightsUpBoulder = async (id) => {
